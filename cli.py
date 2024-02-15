2e6a7c3857dceda776d1a9ce0086011b7734a84c
gal <gal.volkman@gmail.com>
2024-02-15 10:43:08 +0200

    client does everything

--- cli.py
+++ cli.py
@@ -1,7 +1,9 @@
 import cv2
 import threading
-from deepface import DeepFace
 import face_recognition
+import ctypes
+import time
+
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -14,14 +16,22 @@ ref_img = cv2.imread("img.jpg")
 
 
 def check_face(frame):
+    global face_match
 
     known_image = face_recognition.load_image_file("img.jpg")
     unknown_image = frame
-    biden_encoding = face_recognition.face_encodings(known_image)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    known_encoding = face_recognition.face_encodings(known_image)[0]
+    try:
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+    except IndexError:
+        results = [False]
+        print(results)
+        face_match = results[0]
+        return
 
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+    results = face_recognition.compare_faces([known_encoding], unknown_encoding)
     print(results)
+    face_match = results[0]
 
 
 while True:
@@ -37,12 +47,13 @@ while True:
 
         counter += 1
 
-        if face_match:
-            cv2.putText(frame, "match", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, "No Match!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+        if not face_match:
 
-        cv2.imshow("video", frame)
+            ctypes.windll.user32.BlockInput(True)
+            ctypes.windll.user32.LockWorkStation()
+            # time.sleep(1)
+
+        #cv2.imshow("video", frame)
 
 
     key = cv2.waitKey(1)
@@ -53,44 +64,7 @@ cv2.destroyAllWindows()
 counter = 0
 face_match = False
 
-ref_img = cv2.imread("img.jpg")
 
 
-def check_face():
-    global face_match
-    try:
-        if DeepFace.verify(frame, ref_img.copy())['verified']:
-            face_match = True
-        else:
-            face_match = False
-
-    except ValueError:
-        face_match = False
 
 
-while True:
-    ret, frame = cap.read()
-
-    if ret:
-        frame = cv2.flip(frame, 1)
-        if counter % 30 == 0:
-            try:
-                threading.Thread(target=check_face, args=(frame.copy(), )).start()
-            except ValueError:
-                pass
-
-        counter += 1
-
-        if face_match:
-            cv2.putText(frame, "match", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-        else:
-            cv2.putText(frame, "No Match!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-
-        cv2.imshow("video", frame)
-
-
-    key = cv2.waitKey(1)
-    if key == ord("q"):
-        break
-
-cv2.destroyAllWindows()
--- ser.py
+++ ser.py
@@ -1,6 +1,6 @@
-import ctypes
+"""import ctypes
 import time
-print(ctypes.windll.user32.BlockInput(True))
-print(ctypes.windll.user32.LockWorkStation())
+ctypes.windll.user32.BlockInput(True)
+ctypes.windll.user32.LockWorkStation()
 time.sleep(20)
-
+"""
